8a3bfbaca9945fce24fe948ef990cfc137a6c147
pragathi009 <pragathidhk@gmail.com>
2026-07-03 14:18:29 +0530

    Add initial Azure provider configuration

---
+++ provider.tf
@@ -0,0 +1,23 @@
+subscription_id = "31253df5-e027-43d8-8e72-5edcfdc0945f"
+
+resource_group_name ="rg-pragathi"
+
+location = "Central India"
+
+vnet_name = "pragathi-vnet"
+
+subnet_name = "default"
+
+public_ip_name = "pragathi-public-ip"
+
+nic_name = "pragathi-nic"
+
+vm_name = "pragathi-vm"
+
+vm_size = "Standard_B2s"
+
+admin_username = "azureuser"
+
+admin_password = "Password@123456!"
+
+acr_name = "pragathiacr07032026demo"
